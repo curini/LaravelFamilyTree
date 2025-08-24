4f1a3d71c00e6dafd2f43e94deb1e97273077c04
LAPTOP-J9U9GBB8\curin <curini>
2025-08-24 16:35:32 +0200

    add function to detect the admin role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\RolesEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
     protected $fillable = ['name'];
+
+    public function isAdmin(): bool
+    {
+        return $this->id == data_get(Role::where('name', RolesEnum::ADMIN)->first(), 'id');
+    }
 }
