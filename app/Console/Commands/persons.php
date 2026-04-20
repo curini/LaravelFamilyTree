f19a15a4164787230ce1900423746afaf80562a9
LAPTOP-J9U9GBB8\curin <curini>
2026-04-20 17:39:11 +0200

    add new commands
    use for export persons and events

---
+++ app/Console/Commands/persons.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Person;
+use Illuminate\Support\Facades\Storage;
+
+
+class persons extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ft:persons:export {person?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Permet de créer un fichier TypeScript contenant toutes les personnes.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Start of the export');
+        $persons = $this->getPersons();
+        if (empty($persons)) {
+            $this->fail('There is no persons in database!');
+        }
+        Storage::disk('local')->put('my-persons.ts', 'export const myPersons = ' . json_encode($persons));
+        $this->info('End of the export');
+    }
+
+    private function getPersons(): array
+    {
+        return Person::with([
+            'motherPerson:id,first_name,last_name',
+            'fatherPerson:id,first_name,last_name',
+            'spousePerson:id,first_name,last_name',
+            'childrenAsMother:id,first_name,last_name,mother_id',
+            'childrenAsFather:id,first_name,last_name,father_id',
+            'events' => function ($query) {
+                $query->select('id', 'person_id', 'date')
+                    ->orderBy('date', 'asc');
+            },
+        ])
+            ->select(
+                'id',
+                'first_name',
+                'first_names',
+                'last_name',
+                'job',
+                'description',
+                'age',
+                'gender_id',
+                'image_id',
+                'mother_id',
+                'father_id',
+                'spouse_id'
+            )->get()->toArray();
+    }
+}
