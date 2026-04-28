9b9339b07f77417077c61e980814512b8df8c5ef
LAPTOP-J9U9GBB8\curin <curini>
2026-04-28 12:00:22 +0200

    add new command

---
+++ app/Console/Commands/MakeEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('ft:make:email')]
+#[Description('Command description')]
+class MakeEmail extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
