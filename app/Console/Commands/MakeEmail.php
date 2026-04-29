3979e80ca0e2eb3d01e6d1398397452e9d58ac88
LAPTOP-J9U9GBB8\curin <curini>
2026-04-29 14:32:34 +0200

    update command

--- app/Console/Commands/MakeEmail.php
+++ app/Console/Commands/MakeEmail.php
@@ -2,12 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Ai\Agents\EmailGenerator;
+use App\Models\User;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
 
-#[Signature('ft:make:email')]
-#[Description('Command description')]
+#[Signature('ft:make:email {subject} {tone?}')]
+#[Description('Commande permettant de retourner le contenu d\'un email professionnel basé sur un {sujet} et un {ton} choisi. Par défaut, le ton choisi est professionnel.')]
 class MakeEmail extends Command
 {
     /**
@@ -15,6 +17,10 @@ class MakeEmail extends Command
      */
     public function handle()
     {
-        //
+        $sujet = $this->argument('subject');
+        $tone = $this->argument('tone') ?? '';
+        $user = User::where('email', config('app.default_user.email'))->firstOrFail();
+        $response = (new EmailGenerator())->tone($tone)->continueLastConversation($user)->prompt($sujet);
+        $this->info((string)$response);
     }
 }
